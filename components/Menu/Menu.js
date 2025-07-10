2fd814149ca6c61bd18e0b2cfd09b06890cdc2fc
NikosZoros3533 <nikozoro@csd.auth.gr>
2025-07-10 23:57:24 +0300

    animation to menu

--- components/Menu/Menu.js
+++ components/Menu/Menu.js
@@ -1,9 +1,30 @@
 "use client";
 import { useState } from "react";
 import MenuButton from "../MenuButton/MenuButton";
+import { motion, AnimatePresence, stagger } from "framer-motion";
 import Link from "next/link";
 import "../Menu/Menu.css";
 
+const ulVariants = {
+  hidden: {
+    transition: {
+      staggerChildren: 0.05,
+      staggerDirection: -1, // Reverse stagger on exit
+    },
+  },
+  visible: {
+    transition: {
+      staggerChildren: 0.05,
+      staggerDirection: 1, // Normal stagger on enter
+    },
+  },
+};
+
+const liVariants = {
+  hidden: { opacity: 0, x: -30, transition: { duration: 0.1 } },
+  visible: { opacity: 1, x: 0, transition: { type: "spring", stiffness: 200 } },
+  exit: { opacity: 0, transition: { duration: 0.1 } },
+};
 
 export default function Menu() {
   const [isMenuVisible, setIsMenuVisible] = useState(false);
@@ -12,34 +33,58 @@ export default function Menu() {
     setIsMenuVisible((prevStateNavbar) => !prevStateNavbar);
   }
 
-  return <div className="menu-container">
-    <MenuButton menuState={isMenuVisible} onClick={toggleMenu}/>
-    {isMenuVisible && (
-        <nav className="menu">
-          <ul className="menu-list">
-            <li>
-              <Link href="/" onClick={toggleMenu}>Home</Link>
-            </li>
-            <li>
-              <Link href="/couples" onClick={toggleMenu}>Couples</Link>
-            </li>
-            <li>
-              <Link href="/portraits" onClick={toggleMenu}>Portraits</Link>
-            </li>
-            <li>
-              <Link href="/food" onClick={toggleMenu}>Food & Beverages</Link>
-            </li>
-            <li>
-              <Link href="/studio-portraits" onClick={toggleMenu}>Studio</Link>
-            </li>
-            <li>
-              <Link href="/about" onClick={toggleMenu}>About</Link>
-            </li>
-            <li>
-              <Link href="/contact" onClick={toggleMenu}>Contact</Link>
-            </li>
-          </ul>
-        </nav>
-      )}
-  </div>;
+  return (
+    <div className="menu-container">
+      <MenuButton menuState={isMenuVisible} onClick={toggleMenu} />
+      <AnimatePresence>
+        {isMenuVisible && (
+          <nav className="menu">
+            <motion.ul
+              className="menu-list"
+              variants={ulVariants}
+              initial="hidden"
+              animate="visible"
+              exit="hidden"
+            >
+              <motion.li variants={liVariants}>
+                <Link href="/" onClick={toggleMenu}>
+                  Home
+                </Link>
+              </motion.li>
+              <motion.li variants={liVariants}>
+                <Link href="/couples" onClick={toggleMenu}>
+                  Couples
+                </Link>
+              </motion.li>
+              <motion.li variants={liVariants}>
+                <Link href="/portraits" onClick={toggleMenu}>
+                  Portraits
+                </Link>
+              </motion.li>
+              <motion.li variants={liVariants}>
+                <Link href="/food" onClick={toggleMenu}>
+                  Food & Beverages
+                </Link>
+              </motion.li>
+              <motion.li variants={liVariants}>
+                <Link href="/studio-portraits" onClick={toggleMenu}>
+                  Studio
+                </Link>
+              </motion.li>
+              <motion.li variants={liVariants}>
+                <Link href="/about" onClick={toggleMenu}>
+                  About
+                </Link>
+              </motion.li>
+              <motion.li variants={liVariants}>
+                <Link href="/contact" onClick={toggleMenu}>
+                  Contact
+                </Link>
+              </motion.li>
+            </motion.ul>
+          </nav>
+        )}
+      </AnimatePresence>
+    </div>
+  );
 }
